e8edc7c2edb433da54ec8cca92b800a37724ca14
Spatocode <impulseofficial069@gmail.com>
2019-08-26 15:19:29 +0100

    add logger utility function

---
+++ log.go
@@ -0,0 +1,13 @@
+package shiny
+
+import (
+	"log"
+)
+
+// LogError prints an error message to the command line 
+func LogError(reason string, err error) {
+	log.Println("Shiny error: ", reason)
+	if err != nil {
+		log.Println("  Cause:", err)
+	}
+}
