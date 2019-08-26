2feb8767339bcbb09c67dfb80671e503369f6470
Spatocode <impulseofficial069@gmail.com>
2019-08-26 04:09:42 +0100

    make keyboard key constants

---
+++ keyboard.go
@@ -0,0 +1,136 @@
+package shiny
+
+// KeyboardKey represents the pressed key
+type KeyboardKey string
+
+const (
+	// Key0 represents the 0 key
+	Key0 KeyboardKey = "0"
+	// Key1 represents the 1 key
+	Key1 KeyboardKey = "1"
+	// Key2 represents the 2 key
+	Key2 KeyboardKey = "2"
+	// Key3 represents the 3 key
+	Key3 KeyboardKey = "3"
+	// Key4 represents the 4 key
+	Key4 KeyboardKey = "4"
+	// Key5 represents the 5 key
+	Key5 KeyboardKey = "5"
+	// Key6 represents the 6 key
+	Key6 KeyboardKey = "6"
+	// Key7 represents the 7 key
+	Key7 KeyboardKey = "7"
+	// Key8 represents the 8 key
+	Key8 KeyboardKey = "8"
+	// Key9 represents the 9 key
+	Key9 KeyboardKey = "9"
+	// KeyA represents the A key
+	KeyA KeyboardKey = "A"
+	// KeyB represents the B key
+	KeyB KeyboardKey = "B"
+	// KeyC represents the C key
+	KeyC KeyboardKey = "C"
+	// KeyD represents the D key
+	KeyD KeyboardKey = "D"
+	// KeyE represents the E key
+	KeyE KeyboardKey = "E"
+	// KeyF represents the F key
+	KeyF KeyboardKey = "F"
+	// KeyG represents the G key
+	KeyG KeyboardKey = "G"
+	// KeyH represents the H key
+	KeyH KeyboardKey = "H"
+	// KeyI represents the I key
+	KeyI KeyboardKey = "I"
+	// KeyJ represents the J key
+	KeyJ KeyboardKey = "J"
+	// KeyK represents the K key
+	KeyK KeyboardKey = "K"
+	// KeyL represents the L key
+	KeyL KeyboardKey = "L"
+	// KeyM represents the M key
+	KeyM KeyboardKey = "M"
+	// KeyN represents the N key
+	KeyN KeyboardKey = "N"
+	// KeyO represents the O key
+	KeyO KeyboardKey = "O"
+	// KeyP represents the P key
+	KeyP KeyboardKey = "P"
+	// KeyQ represents the Q key
+	KeyQ KeyboardKey = "Q"
+	// KeyR represents the R key
+	KeyR KeyboardKey = "R"
+	// KeyS represents the S key
+	KeyS KeyboardKey = "S"
+	// KeyT represents the T key
+	KeyT KeyboardKey = "T"
+	// KeyU represents the U key
+	KeyU KeyboardKey = "U"
+	// KeyV represents the V key
+	KeyV KeyboardKey = "V"
+	// KeyW represents the W key
+	KeyW KeyboardKey = "W"
+	// KeyX represents the X key
+	KeyX KeyboardKey = "X"
+	// KeyY represents the Y key
+	KeyY KeyboardKey = "Y"
+	// KeyZ represents the Z key
+	KeyZ KeyboardKey = "Z"
+
+	// KeyF1 represents the F1 key
+	KeyF1 KeyboardKey = "F1"
+	// KeyF2 represents the F2 key
+	KeyF2 KeyboardKey = "F2"
+	// KeyF3 represents the F3 key
+	KeyF3 KeyboardKey = "F3"
+	// KeyF4 represents the F4 key
+	KeyF4 KeyboardKey = "F4"
+	// KeyF5 represents the F5 key
+	KeyF5 KeyboardKey = "F5"
+	// KeyF6 represents the F6 key
+	KeyF6 KeyboardKey = "F6"
+	// KeyF7 represents the 7 key
+	KeyF7 KeyboardKey = "F7"
+	// KeyF8 represents the F8 key
+	KeyF8 KeyboardKey = "F8"
+	// KeyF9 represents the F9 key
+	KeyF9 KeyboardKey = "F9"
+	// KeyF10 represents the F10 key
+	KeyF10 KeyboardKey = "F10"
+	// KeyF11 represents the F11 key
+	KeyF11 KeyboardKey = "F11"
+	// KeyF12 represents the F12 key
+	KeyF12 KeyboardKey = "F12"
+
+	// KeyEnter represents the enter key
+	KeyEnter KeyboardKey = "Enter"
+	// KeyEscape represents the esc key
+	KeyEscape KeyboardKey = "Escape"
+	// KeyReturn represents the carriage return key
+	KeyReturn KeyboardKey = "Return"
+	// KeyBackSpace represents the backspace key
+	KeyBackSpace KeyboardKey = "Backspace"
+	// KeyTab represents the tab key
+	KeyTab KeyboardKey = "Tab"
+	// KeyUp represents the up arrow key key
+	KeyUp KeyboardKey = "Up"
+	// KeyDown represents the down arrow key
+	KeyDown KeyboardKey = "Down"
+	// KeyLeft represents the left arrow key
+	KeyLeft KeyboardKey = "Left"
+	// KeyRight represents the right arrow key
+	KeyRight KeyboardKey = "Right"
+
+	// KeyPageUp represents the page up numeric key
+	KeyPageUp KeyboardKey = "Prior"
+	// KeyPageDown represents the page down numeric key
+	KeyPageDown KeyboardKey = "Next"
+	// KeyInsert represents the insert mode key
+	KeyInsert KeyboardKey = "Insert"
+	// KeyDelete represents the delete-after-cursor key
+	KeyDelete KeyboardKey = "Delete"
+	// KeyHome represents the line home key
+	KeyHome KeyboardKey = "Home"
+	// KeyEnd represents the line end key
+	KeyEnd KeyboardKey = "End"
+)
