1d101cdf692e1b41456b82534e734f7d10015347
Spatocode <impulseofficial069@gmail.com>
2019-08-29 21:43:52 +0100

    fix nasty typos

--- internal/drivers/keyboard.go
+++ internal/drivers/keyboard.go
@@ -5,28 +5,28 @@ import (
 	"github.com/spatocode/drone"
 )
 
-func keyboardTranslate(key gflw.Key) drone.KeyboardKey {
+func keyboardTranslate(key glfw.Key) drone.KeyboardKey {
 	switch key {
 	case glfw.Key0:
-		return drone.Key0:
+		return drone.Key0
 	case glfw.Key1:
-		return drone.Key1:
+		return drone.Key1
 	case glfw.Key2:
-		return drone.Key2:
+		return drone.Key2
 	case glfw.Key3:
-		return drone.Key3:
+		return drone.Key3
 	case glfw.Key4:
-		return drone.Key4:
+		return drone.Key4
 	case glfw.Key5:
-		return drone.Key5:
+		return drone.Key5
 	case glfw.Key6:
-		return drone.Key6:
+		return drone.Key6
 	case glfw.Key7:
-		return drone.Key7:
+		return drone.Key7
 	case glfw.Key8:
-		return drone.Key8:
+		return drone.Key8
 	case glfw.Key9:
-		return drone.Key9:
+		return drone.Key9
 	default:
 		return ""
 	}
