84e08175b34089baa382110fce45a932ad9a94f3
Spatocode <impulseofficial069@gmail.com>
2019-08-31 22:38:00 +0200

    implement window interface

--- internal/drivers/glfw/windows.go
+++ internal/drivers/glfw/windows.go
@@ -1,12 +1,31 @@
 package glfw
 
 import (
-	_"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/spatocode/drone"
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
+
+var _ drone.Window = (*Window)(nil)
 
 type Window struct {
 	width     int
 	height    int
 	resizable bool
 	title     string
+}
+
+func (window *Window) Title() string{
+	return window.title
+}
+
+func (window *Window) Resizable(bo bool) {
+	window.resizable = bo
+}
+
+func (window *Window) Run() {
+	if window.resizable {
+		glfw.WindowHint(glfw.Resizable, glfw.True)
+	} else {
+		glfw.WindowHint(glfw.Resizable, glfw.False)
+	}
 }
